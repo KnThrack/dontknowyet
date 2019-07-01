a29688f6e5e23a273cbb6b00be8dfe0b1a6da376
Kay Degro <kay.degro@ecenta.com>
2019-07-01 10:25:23 +0200

    controlled inputs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ class App extends Component {
     super(props);
     this.state = { apiResponse: "" };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
+
 
   callAPI() {
     fetch("https://notsureyetapp.herokuapp.com/api/recipes")
@@ -29,6 +31,17 @@ class App extends Component {
     this.callAPI();
   }
 
+  // handlers
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     const form = document.getElementById('bigform');
@@ -41,6 +54,8 @@ class App extends Component {
  */
   }
 
+  // handlers end 
+  
   render() {
     if (this.state.recipes) {
       return (
--- src/views/recipes.js
+++ src/views/recipes.js
@@ -11,16 +11,16 @@ const Recipes = ({ recipes, that }) => {
             {
                 recipes.data.map((recipe) =>
                     <div key={recipe._id.toString()}>
-                        <Form id="bigform" onChange={that.handleSubmit} /*controlId={recipe._id.toString()+".Form"}*/>
+                        <Form id="bigform" /*controlId={recipe._id.toString()+".Form"}*/>
                             <Form.Group /*controlId={recipe._id.toString()+".ControlInput1"}*/>
                                 <Form.Label htmlFor="title" >Recipe Title</Form.Label>
-                                <Form.Control id="title" value={recipe.title} />
+                                <Form.Control onChange={that.handleInputChange} id="title" value={recipe.title} />
                                 <Form.Label htmlFor="name">Recipe Name</Form.Label>
-                                <Form.Control id="name" value={recipe.name} />
+                                <Form.Control onChange={that.handleInputChange} id="name" value={recipe.name} />
                             </Form.Group>
                             <Form.Group /*controlId={recipe._id.toString()+".ControlCuisine"}*/>
                                 <Form.Label htmlFor="cuisine" >Cuisine</Form.Label>
-                                <Form.Control id="cuisine" value={recipe.cuisine} as="select">
+                                <Form.Control onChange={that.handleInputChange} id="cuisine" value={recipe.cuisine} as="select">
                                     <option>German</option>
                                     <option>Chinease</option>
                                     <option>Asian</option>
@@ -30,7 +30,7 @@ const Recipes = ({ recipes, that }) => {
                             </Form.Group>
                             <Form.Group /*controlId={recipe._id.toString()+".ControlTextarea1"}*/>
                                 <Form.Label htmlFor="recipe">Recipe</Form.Label>
-                                <Form.Control id="recipe" as="textarea" rows="10" value={recipe.recipe}/>
+                                <Form.Control onChange={that.handleInputChange} id="recipe" as="textarea" rows="10" value={recipe.recipe}/>
                             </Form.Group>
                         </Form>
                     </div>
