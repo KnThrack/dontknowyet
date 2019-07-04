a2f9b2f245c7da6c66807a4598d537edd0668f58
Kay Degro <kay.degro@ecenta.com>
2019-07-04 15:24:44 +0200

    g

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,11 +1,11 @@
 // src/components/NavBar.js
 
 import React from "react";
-import { useAuth0 } from "../react-auth0-wrapper";
+import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     return (
         <div>
