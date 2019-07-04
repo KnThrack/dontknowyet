18de7a20a53241ad01c2c758962e7ad0bb482acb
Kay Degro <kay.degro@ecenta.com>
2019-07-04 15:02:24 +0200

    test

--- src/Base.js
+++ src/Base.js
@@ -4,9 +4,8 @@ import App from "./App";
 import { useAuth0 } from "./react-auth0-spa";
 
 // styles
-import "samples-bootstrap-theme/dist/css/auth0-theme.css";
+//import "samples-bootstrap-theme/dist/css/auth0-theme.css";
 import "./App.css";
-
 /* //fontawesome
 import initFontAwesome from "./utils/initFontAwesome";
 initFontAwesome();
