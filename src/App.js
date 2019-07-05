b850879b2e69aef5a1671ce0bffe5d2bf69876a2
Kay Degro <kay.degro@ecenta.com>
2019-07-05 13:20:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -24,11 +24,6 @@ class App extends Component {
     this.handleTableChange = this.handleTableChange.bind(this);
     this.handleAddRecipe = this.handleAddRecipe.bind(this);
 
-    
-  // set auth headers
-  axios.defaults.headers.post['Authorization'] = 'Bearer ' + props.token;
-  axios.defaults.headers.get['Authorization'] = 'Bearer ' + props.token;
-  axios.defaults.headers.put['Authorization'] = 'Bearer ' + props.token;
   
   // set the default axios stuff
   axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -38,6 +33,12 @@ class App extends Component {
 
 
   async callAPI() {
+
+      // set auth headers
+  axios.defaults.headers.post['Authorization'] = 'Bearer ' + this.props.token;
+  axios.defaults.headers.get['Authorization'] = 'Bearer ' + this.props.token;
+  axios.defaults.headers.put['Authorization'] = 'Bearer ' + this.props.token;
+
     axios.get("https://notsureyetapp.herokuapp.com/");
     // get the initial recipes
     const recipes = (await axios.get("https://notsureyetapp.herokuapp.com/api/recipes")).data;
