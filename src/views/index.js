7f2fb1e06395b54e7495cff42f35784b86fc310e
Kay Degro <kay.degro@ecenta.com>
2019-07-10 15:48:45 +0200

    index for views

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export { Loading } from './Loading';
+export { deleteModal } from './deleteModal';
+export { NavBar } from './NavBar';
+export { PrivateRoute } from './PrivateRoute';
+export { Profile } from './Profile';
+export { recipe } from './recipe';
+export { recipes } from './recipes';
