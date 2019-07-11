b0b11f2b4948693a90e31b7a4edfc909a2e63b20
Kay Degro <kay.degro@ecenta.com>
2019-07-11 15:50:37 +0200

    navbar

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 
