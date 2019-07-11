f5255267ae877e897343784646055c40511ce8b1
Kay Degro <kay.degro@ecenta.com>
2019-07-11 10:34:52 +0200

    modal test

--- src/views/deleteModal.js
+++ src/views/deleteModal.js
@@ -4,7 +4,7 @@ import React from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-const DeleteModal = ({ showModal }, ...props) => {
+const DeleteModal = (...props) => {
 
     function handleClose() {
         props.handleModalClose();
