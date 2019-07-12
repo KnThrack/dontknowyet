d5d8d601570bac85bb2b078368b7829ef9a7c23c
Kay Degro <kay.degro@ecenta.com>
2019-07-12 11:06:10 +0200

    lala

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,43 +1,50 @@
 // src/views/NavBar.js
 
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
-import Image from 'react-bootstrap/Image'
+import Image from "react-bootstrap/Image";
 
 const NavBar = () => {
-    const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+	const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-    let userpicture = "../assets/circle.svg"
-    if (isAuthenticated) { if (user.picture) { userpicture = user.picture } };
+	let userpicture = "../assets/circle.svg";
+	if (isAuthenticated) {
+		if (user.picture) {
+			userpicture = user.picture;
+		}
+	}
 
-    //in your component
+	//in your component
 
-    function loginout() {
-        !isAuthenticated && loginWithRedirect({});
-        isAuthenticated && logout();
-    }
+	function loginout() {
+		!isAuthenticated && loginWithRedirect({});
+		isAuthenticated && logout();
+	}
 
-    return (
-        <Navbar expand="lg">
-            <Navbar.Brand href="/">Dontknowyet</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end" justify="true" >
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/profile">Profile</Nav.Link>
-                </Nav>
-                <Image onClick={loginout} src={userpicture} roundedCircle height="50px" />
-            </Navbar.Collapse>
-        </Navbar>
-    );
+	return (
+		<Navbar expand='lg'>
+			<Navbar.Brand href='/'>Dontknowyet</Navbar.Brand>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav className='justify-content-end' justify='true'>
+					<Nav.Link href='/'>Home</Nav.Link>
+					<Nav.Link href='/profile'>Profile</Nav.Link>
+				</Nav>
+				<Image
+					onClick={loginout}
+					src={userpicture}
+					roundedCircle
+					height='50px'
+				/>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export { NavBar };
-
-
 
 /*
                         <Navbar.Text>
