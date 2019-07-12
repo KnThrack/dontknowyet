3aa08568ce414466546ff01b5ede118765a0585d
Kay Degro <kay.degro@ecenta.com>
2019-07-12 09:21:14 +0200

    navbar

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -5,13 +5,51 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
+import Image from 'react-bootstrap/Image'
 
 const NavBar = () => {
     const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
+    //in your component
+    function addDefaultSrc(ev) {
+        ev.target.src = "../assets/circle.svg";
+    }
+    function loginout() {
+        !isAuthenticated && loginWithRedirect({});
+        isAuthenticated && logout();
+    }
+
     return (
         <div>
-            {!isAuthenticated && (
+            <Navbar expand="lg">
+                <Navbar.Brand href="/">Dontknowyet</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">
+                            Home
+                            </Nav.Link>
+                        <Nav.Link href="/profile">
+                            <Link to="/profile">Profile</Link>
+                        </Nav.Link>
+                    </Nav>
+                    <Image onError={addDefaultSrc} src={user.picture} rounded onClick={loginout}> </Image>/>
+                    </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export { NavBar };
+
+
+
+/*
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">{user.name}</a>
+                        </Navbar.Text>
+
+          {!isAuthenticated && (
                 <button
                     onClick={() =>
                         loginWithRedirect({})
@@ -23,30 +61,9 @@ const NavBar = () => {
 
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
 
-            {/* NEW - add a link to the home and profile pages */}
+            { NEW - add a link to the home and profile pages }
             {isAuthenticated && (
 
-                <Navbar expand="lg">
-                    <Navbar.Brand href="/">Dontknowyetapp Recipe </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/">
-                                <Link to="/">Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="/profile">
-                                <Link to="/profile">Profile</Link>
-                            </Nav.Link>
-                        </Nav>
-                        <Navbar.Text>
-                            Signed in as: <a href="#login">{user.name}</a>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Navbar>
-
-            )}
-        </div>
-    );
-};
-
-export { NavBar };
+... DIV
+  )}
+                        */
