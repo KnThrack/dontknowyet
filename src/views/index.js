9b6a9a2e08acde1b40c1bad07bce33e772d50175
Kay Degro <kay.degro@ecenta.com>
2019-07-15 15:51:15 +0200

    fix

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,7 @@
-export { Loading } from "./Loading.js";
-export { ConfirmationModal } from "./ConfirmationModal.js";
-export { NavBar } from "./NavBar.js";
-export { PrivateRoute } from "./PrivateRoute.js";
-export { Profile } from "./Profile.js";
-export { Recipe } from "./Recipe.js";
-export { Recipes } from "./Recipes.js";
+export { Loading } from "./Loading";
+export { ConfirmationModal } from "./ConfirmationModal";
+export { NavBar } from "./NavBar";
+export { PrivateRoute } from "./PrivateRoute";
+export { Profile } from "./Profile";
+export { Recipe } from "./Recipe";
+export { Recipes } from "./Recipes";
