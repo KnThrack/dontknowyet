31dd60dae472e75460887a620981e352eaeddc72
Kay Degro <kay.degro@ecenta.com>
2019-07-17 14:49:23 +0200

    l

--- src/views/index.js
+++ src/views/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export { Loading } from "./Loading.js";
-export { ConfirmationModal } from "./ConfirmationModal.js";
-export { NavBar } from "./NavBar.js";
-export { PrivateRoute } from "./PrivateRoute.js";
-export { Profile } from "./Profile.js";
-export { Recipe } from "./Recipe.js";
-export { Recipes } from "./Recipes.js";
-=======
 export { Loading } from "./Loading";
 export { ConfirmationModal } from "./ConfirmationModal";
 export { NavBar } from "./NavBar";
@@ -14,4 +5,3 @@ export { PrivateRoute } from "./PrivateRoute";
 export { Profile } from "./Profile";
 export { Recipe } from "./Recipe";
 export { Recipes } from "./Recipes";
->>>>>>> 190e300212fd938b2a4b0174aef9ab72a188a47d
