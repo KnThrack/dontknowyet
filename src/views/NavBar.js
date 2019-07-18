283f6cb351223df526c5a63cc9a67113d9ae8596
Kay Degro <kay.degro@ecenta.com>
2019-07-18 09:20:02 +0200

    logo

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { useAuth0 } from "../react-auth0-spa";
 import Image from "react-bootstrap/Image";
-//import { ReactComponent as Logo } from "../assets/pandalogo.svg";
+import { ReactComponent as Logo } from "../assets/Logo2small.svg";
 
 const NavBar = () => {
 	const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -27,7 +27,7 @@ const NavBar = () => {
 	return (
 		<Navbar expand='lg'>
 			<Navbar.Brand href='/'>
-				<img src='pandalogo.png' width='50' height='50' className='d-inline-block align-top' alt=""/>
+				<Logo width='50' height='50' className='d-inline-block align-top'/>
 				{" Dontknowyet"}
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls='basic-navbar-nav' />
