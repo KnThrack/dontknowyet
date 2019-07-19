1de6e2f556bde0361be5a396d4ef7437a5e2a79f
Kay Degro <kay.degro@ecenta.com>
2019-07-19 10:44:12 +0200

    l

--- src/views/Recipe.js
+++ src/views/Recipe.js
@@ -70,7 +70,7 @@ const Recipe = (...props) => {
 									<Td>{ingredient.unit}</Td>
 									<Td width="50px" >
 										<img
-										    id={ingredient._id.toString()}
+										    id={ingredient._id ? ingredient._id : 1}
 											onClick={handleDeleteIngredient}
 											src='https://unicons.iconscout.com/release/v1.0.0/svg/multiply.svg'
 											alt=''
