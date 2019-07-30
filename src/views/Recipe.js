4baf761604fc6ca048dc6bed2ba478c20277a42d
Kay Degro <kay.degro@ecenta.com>
2019-07-30 14:13:45 +0200

    make form a card

--- src/views/Recipe.js
+++ src/views/Recipe.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Upload, PictureCarousel } from "./";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import Card from "react-bootstrap/Card";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import "./table.css";
@@ -53,80 +53,85 @@ const Recipe = (...props) => {
 	if (myRecipe === null) return <p>Loading ...</p>;
 	return (
 		<div key={myRecipe._id.toString()} className='container'>
-			<Form id='bigform' /*controlId={recipe._id.toString()+".Form"}*/>
-				<Form.Group /*controlId={recipe._id.toString()+".ControlInput1"}*/>
-					<Form.Label htmlFor='title'>Recipe Title</Form.Label>
-					<Form.Control name='title' onChange={handleInputChanges} id='title' value={myRecipe.title} />
-					<Form.Label htmlFor='name'>Recipe Name</Form.Label>
-					<Form.Control name='name' onChange={handleInputChanges} id='name' value={myRecipe.name} />
-					<Form.Label htmlFor='cuisine'>Cuisine</Form.Label>
-					<Form.Control name='cuisine' onChange={handleInputChanges} id='cuisine' value={myRecipe.cuisine} as='select'>
-						<option>German</option>
-						<option>Chinese</option>
-						<option>Asian</option>
-						<option>French</option>
-						<option>Italian</option>
-					</Form.Control>
-				</Form.Group>
-				<Form.Group /*controlId={recipe._id.toString()+".ControlTextarea1"}*/>
-					<Form.Label htmlFor='recipe'>Recipe</Form.Label>
-					<Form.Control name='recipe' onChange={handleInputChanges} id='recipe' as='textarea' rows='10' value={myRecipe.recipe} />
-				</Form.Group>
-				<Form.Group className='Recipe-pictures'>
-					<Upload
-						successfullUploaded={successfullUploaded}
-						uploadProgress={uploadProgress}
-						uploading={uploading}
-						files={files}
-						setFiles={setFiles}
-						setSuccessfullUploaded={setSuccessfullUploaded}
-						onFilesAdded={onFilesAdded}
-						uploadFiles={uploadFiles}
-					/>
-					<PictureCarousel pictureList={pictureList} recipeID={myRecipe._id.toString()} />
-				</Form.Group>
-				<Form.Group>
-					<Table striped='true' bordered='true' hover='true'>
-						<Thead>
-							<Tr>
-								<Th>Ingredient</Th>
-								<Th>Quantity</Th>
-								<Th>Unit</Th>
-								<Th width='50px' />
-							</Tr>
-						</Thead>
-						{myRecipe.ingredients.map(ingredient => (
-							<Tbody key={ingredient._id ? ingredient._id : 1}>
-								<Tr onClick={handleChangeIngredient} key={ingredient._id ? ingredient._id : 1} id={ingredient._id ? ingredient._id : 1}>
-									<Td>{ingredient.ingredient}</Td>
-									<Td>{ingredient.quantity}</Td>
-									<Td>{ingredient.unit}</Td>
-									<Td className='ingredientDeleteCell' width='50px'>
-										<img
-											id={ingredient._id ? ingredient._id : 1}
-											onClick={handleDeleteIngredient}
-											src='https://unicons.iconscout.com/release/v1.0.0/svg/multiply.svg'
-											alt=''
-											width='30'
-											height='30'
-											className='ingredientDelete-IMG d-inline-block align-center'
-										/>
-										<Button
-											id={ingredient._id ? ingredient._id : 1}
-											type='submit'
-											className='ingredientDelete-Button submit-Button'
-											variant='primary'
-											onClick={handleDeleteIngredient}
-										>
-											Delete
-										</Button>
-									</Td>
-								</Tr>
-							</Tbody>
-						))}
-					</Table>
-				</Form.Group>
-			</Form>
+			<Card className={cardClass(recipe)} key={recipe._id.toString()}>
+				<Card.Header>{recipe.cuisine}</Card.Header>
+				<Card.Body>
+					<Form id='bigform' /*controlId={recipe._id.toString()+".Form"}*/>
+						<Form.Group /*controlId={recipe._id.toString()+".ControlInput1"}*/>
+							<Form.Label htmlFor='title'>Recipe Title</Form.Label>
+							<Form.Control name='title' onChange={handleInputChanges} id='title' value={myRecipe.title} />
+							<Form.Label htmlFor='name'>Recipe Name</Form.Label>
+							<Form.Control name='name' onChange={handleInputChanges} id='name' value={myRecipe.name} />
+							<Form.Label htmlFor='cuisine'>Cuisine</Form.Label>
+							<Form.Control name='cuisine' onChange={handleInputChanges} id='cuisine' value={myRecipe.cuisine} as='select'>
+								<option>German</option>
+								<option>Chinese</option>
+								<option>Asian</option>
+								<option>French</option>
+								<option>Italian</option>
+							</Form.Control>
+						</Form.Group>
+						<Form.Group /*controlId={recipe._id.toString()+".ControlTextarea1"}*/>
+							<Form.Label htmlFor='recipe'>Recipe</Form.Label>
+							<Form.Control name='recipe' onChange={handleInputChanges} id='recipe' as='textarea' rows='10' value={myRecipe.recipe} />
+						</Form.Group>
+						<Form.Group className='Recipe-pictures'>
+							<Upload
+								successfullUploaded={successfullUploaded}
+								uploadProgress={uploadProgress}
+								uploading={uploading}
+								files={files}
+								setFiles={setFiles}
+								setSuccessfullUploaded={setSuccessfullUploaded}
+								onFilesAdded={onFilesAdded}
+								uploadFiles={uploadFiles}
+							/>
+							<PictureCarousel pictureList={pictureList} recipeID={myRecipe._id.toString()} />
+						</Form.Group>
+						<Form.Group>
+							<Table striped='true' bordered='true' hover='true'>
+								<Thead>
+									<Tr>
+										<Th>Ingredient</Th>
+										<Th>Quantity</Th>
+										<Th>Unit</Th>
+										<Th width='50px' />
+									</Tr>
+								</Thead>
+								{myRecipe.ingredients.map(ingredient => (
+									<Tbody key={ingredient._id ? ingredient._id : 1}>
+										<Tr onClick={handleChangeIngredient} key={ingredient._id ? ingredient._id : 1} id={ingredient._id ? ingredient._id : 1}>
+											<Td>{ingredient.ingredient}</Td>
+											<Td>{ingredient.quantity}</Td>
+											<Td>{ingredient.unit}</Td>
+											<Td className='ingredientDeleteCell' width='50px'>
+												<img
+													id={ingredient._id ? ingredient._id : 1}
+													onClick={handleDeleteIngredient}
+													src='https://unicons.iconscout.com/release/v1.0.0/svg/multiply.svg'
+													alt=''
+													width='30'
+													height='30'
+													className='ingredientDelete-IMG d-inline-block align-center'
+												/>
+												<Button
+													id={ingredient._id ? ingredient._id : 1}
+													type='submit'
+													className='ingredientDelete-Button submit-Button'
+													variant='primary'
+													onClick={handleDeleteIngredient}
+												>
+													Delete
+												</Button>
+											</Td>
+										</Tr>
+									</Tbody>
+								))}
+							</Table>
+						</Form.Group>
+					</Form>
+				</Card.Body>
+			</Card>
 		</div>
 	);
 };
