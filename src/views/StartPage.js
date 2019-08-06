298cf946b7e7f5a873ff21fa74bf9afb6cad8fd4
Kay Degro <kay.degro@ecenta.com>
2019-08-06 16:17:36 +0200

    App

--- src/views/StartPage.js
+++ src/views/StartPage.js
@@ -3,7 +3,7 @@ import Background from "../assets/background.svg";
 
 const StartPage = () => {
 	return (
-		<div height='1000px' background={Background}>
+		<div class='App-Home'>
 			<h1>Hello world!</h1>
 		</div>
 	);
